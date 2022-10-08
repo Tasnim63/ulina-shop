0f3e474562d476a73ca9a3db30d188ed79218721
tamanna <tamanna202163@gmail.com>
2022-10-08 10:27:34 +0600

    signup pages added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Col, Container, FormGroup, Row } from "reactstrap";
 import '../styles/login.css';
 import Helmet from "../components/Helmet/Helmet";
 export default function Login() {
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
   return (
     <Helmet title="Login">
       <section className=" py-5">
         <Container>
           <Row>
             <Col lg="6"  className=" m-auto text-center">
-            <h3 className=" fw-bold fs-4">Login</h3>
+            <h3 className=" fw-bold fs-4 py-3">Login</h3>
             <form className=" auth_form">
               <FormGroup className=" form-group">
-                <input type="email" placeholder="Enter your email" />
+                <input className=" w-100" type="email" placeholder="Enter your email" value={email} onChange={e => setEmail(e.target.value)}/>
               </FormGroup>
               <FormGroup className=" form-group">
-                <input type="password" placeholder="Enter your password" />
+                <input className=" w-100"  type="password" placeholder="Enter your password"  value={password} onChange={e => setPassword(e.target.value)} />
               </FormGroup>
-              <button className=" buy_btn auth_btn">Login</button>
-             <p> Don't  have an account?  <Link to="/signup"> Create an account</Link></p>
+              <button type="submit" className="w-100 mt-3 buy_btn auth_btn">Login</button>
+             <p className=" text-white pt-4"> Don't  have an account?  <Link to="/signup"> Create an account</Link></p>
             </form>
             </Col>
           </Row>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
 
+
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Col, Container, FormGroup, Row } from "reactstrap";
+import '../styles/login.css';
+import Helmet from "../components/Helmet/Helmet";
 export default function Signup() {
+  const [username,setUsername] = useState('');
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
   return (
-    <div>Signup</div>
-  )
+    <Helmet title="signup">
+      <section className=" py-5">
+        <Container>
+          <Row>
+            <Col lg="6"  className=" m-auto text-center">
+            <h3 className=" fw-bold fs-4 py-3">SignUp</h3>
+            <form className=" auth_form">
+              <FormGroup className=" form-group">
+                <input className=" w-100" type="text" placeholder="Enter your name" value={username} onChange={e => setUsername(e.target.value)}/>
+              </FormGroup>
+              <FormGroup className=" form-group">
+                <input className=" w-100" type="email" placeholder="Enter your email" value={email} onChange={e => setEmail(e.target.value)}/>
+              </FormGroup>
+              <FormGroup className=" form-group">
+                <input className=" w-100"  type="password" placeholder="Enter your password"  value={password} onChange={e => setPassword(e.target.value)} />
+              </FormGroup>
+              <button type="submit" className="w-100 mt-3 buy_btn auth_btn">create an account</button>
+             <p className=" text-white pt-4">Already   have an account?  <Link to="/login"> login now</Link></p>
+            </form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 }
