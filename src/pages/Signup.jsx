5602795613be278f2ad97dcd9e11d94d5883eb74
tamanna <tamanna202163@gmail.com>
2022-10-08 13:22:37 +0600

    file added

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -9,6 +9,8 @@ export default function Signup() {
   const [username,setUsername] = useState('');
   const [email,setEmail] = useState('');
   const [password,setPassword] = useState('');
+  const [file,setFile] = useState(null);
+
   return (
     <Helmet title="signup">
       <section className=" py-5">
@@ -24,7 +26,9 @@ export default function Signup() {
                 <input className=" w-100" type="email" placeholder="Enter your email" value={email} onChange={e => setEmail(e.target.value)}/>
               </FormGroup>
               <FormGroup className=" form-group">
-                <input className=" w-100"  type="password" placeholder="Enter your password"  value={password} onChange={e => setPassword(e.target.value)} />
+                <input className=" w-100"  
+                type="file" 
+                 onChange={e => setFile(e.target.files[0])} />
               </FormGroup>
               <button type="submit" className="w-100 mt-3 buy_btn auth_btn">create an account</button>
              <p className=" text-white pt-4">Already   have an account?  <Link to="/login"> login now</Link></p>
